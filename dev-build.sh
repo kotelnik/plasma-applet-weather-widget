eacca5bb946493489b357a1ffc0f1044b5e8c382
Kotelnik <clearmartin@seznam.cz>
2016-01-07 21:48:44 +0100

    create plasmoid package shell added

--- dev-build.sh
+++ dev-build.sh
@@ -5,6 +5,7 @@ cd $DIR
 sudo rm -r build
 mkdir build
 cd build
+
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DLIB_INSTALL_DIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
 make
 sudo make install
